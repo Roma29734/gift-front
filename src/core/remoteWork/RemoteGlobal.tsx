c5c45cd846cb41badd8f69189b8f31852d1bf56d
Roma <163242467+roma29733@users.noreply.github.com>
2024-11-10 16:43:56 +0300

    mn

--- src/core/remoteWork/RemoteGlobal.tsx
+++ src/core/remoteWork/RemoteGlobal.tsx
@@ -1,4 +1,4 @@
 // import {retrieveLaunchParams} from "@telegram-apps/sdk";
 
 export const initDataRaw = "1"
-export const BASE_URL = 'https://arabic-licenses-entire-catalog.trycloudflare.com/api/'
+export const BASE_URL = 'https://persistent-readers-common-republicans.trycloudflare.com/api/'
