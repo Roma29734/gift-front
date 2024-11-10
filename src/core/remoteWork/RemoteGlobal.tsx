c4edfd075716a5014d5e76f481deea86ccd75ac1
Roma <163242467+roma29733@users.noreply.github.com>
2024-11-10 16:28:54 +0300

    m

--- src/core/remoteWork/RemoteGlobal.tsx
+++ src/core/remoteWork/RemoteGlobal.tsx
@@ -1,4 +1,4 @@
 // import {retrieveLaunchParams} from "@telegram-apps/sdk";
 
 export const initDataRaw = "1"
-export const BASE_URL = 'https://8da5-2a0b-4140-a891-00-2.ngrok-free.app/api/'
+export const BASE_URL = 'https://04d2-2a0b-4140-a891-00-2.ngrok-free.app/api/'
