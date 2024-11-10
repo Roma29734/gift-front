25bb480daaa013b95b9861af46369b258e7da642
Roma <163242467+roma29733@users.noreply.github.com>
2024-11-10 16:25:58 +0300

    m

--- src/core/remoteWork/RemoteGlobal.tsx
+++ src/core/remoteWork/RemoteGlobal.tsx
@@ -1,4 +1,4 @@
 // import {retrieveLaunchParams} from "@telegram-apps/sdk";
 
 export const initDataRaw = "1"
-export const BASE_URL = 'https://exhibition-clubs-mb-margaret.trycloudflare.com/api/'
+export const BASE_URL = 'https://8da5-2a0b-4140-a891-00-2.ngrok-free.app/api/'
